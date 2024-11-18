5d5e50d1f0f65609e9ee43c744a01fcb41397b33
Mehboob Alam <mehboobalam786461@gmail.com>
2024-11-18 18:21:35 +0500

    SignUp and logIn api for user is completed , Now , I will go to verifcation through email.

--- controller/email/configration.js
+++
@@ -1,8 +0,0 @@
-const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
-    auth: {
-        user: 'ari.boehm9@ethereal.email',
-        pass: 'GbrmydnEK5YCQp5mCC'
-    }
-});
