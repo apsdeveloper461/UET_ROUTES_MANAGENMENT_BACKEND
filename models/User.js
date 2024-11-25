df679c238f0d8c4e422e1dfcc05d640cf7def362
Mehboob Alam <mehboobalam786461@gmail.com>
2024-11-25 08:56:24 +0500

    login page error fixed

--- models/User.js
+++ models/User.js
@@ -6,7 +6,6 @@ const userSchema=new mongoose.Schema({
     email:{type:String,required:true,unique:true},
     password:{type:String,required:true},
     isVerified:{type:Boolean,default:false},
-
 })
 
 
